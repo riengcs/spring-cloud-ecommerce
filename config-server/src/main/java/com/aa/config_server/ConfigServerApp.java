3a1af1958cadd1b3cb049ee42df10c4a20263d3a
riengcs <riengcs@gmail.com>
2025-08-28 15:31:39 +0700

    Initial commit

--- config-server/src/main/java/com/aa/config_server/ConfigServerApp.java
+++ config-server/src/main/java/com/aa/config_server/ConfigServerApp.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApp.class, args);
     }
 
 }
